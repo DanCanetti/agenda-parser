6df3da9ff5af2be9ab47b5db62d17d46e448a7d9
Dan Canetti <info@danielcanetti.com>
2020-10-24 23:18:53 +0100

    Testing json - use online tool to convert

--- js/parse-csv.js
+++ js/parse-csv.js
@@ -1,10 +1,4 @@
 // Convert CSV to Json
-let csvToJson = require('convert-csv-to-json');
- 
-let fileInputName = 'csv/test.csv'; 
-let fileOutputName = 'csv/test.json';
- 
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
 
 // Parse Json File
 new Vue({
